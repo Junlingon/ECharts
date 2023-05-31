eda5dc93e24b059be084717a2c72c46573c2f1b5
JunlinGong <913756523@qq.com>
2023-05-31 23:07:35 +0800

    feat: 搭建路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './router/routes';
 function App() {
 
   return (
-    <>
-      init
-    </>
+    <Router>
+      <AppRoutes></AppRoutes>
+    </Router>
   );
 }
 
